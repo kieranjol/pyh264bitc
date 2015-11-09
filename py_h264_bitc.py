4c186fc0b6d01286c4ce40226e3e2ce992af30b7
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-09 21:57:55 +0000

    reverted horizontal positioon

--- py_h264_bitc.py
+++ py_h264_bitc.py
@@ -43,13 +43,13 @@ for filename in video_files: #loop all files in directory
 	
 	# Calculate x and y coordinates of the timecode
 	vertical_position_timecode = video_height / 1.2
-	horizontal_position_timecode = video_width / 3
+	horizontal_position_timecode = video_width / 2
 	
 	# Calculate appropriate font size
 	font_size = video_height / 12
 	#pdb.set_trace()
 	
-	textoptions = ("fontsize=%d:x=%d:y=%d" % (font_size,horizontal_position_timecode,vertical_position_timecode))
+	textoptions = ("fontsize=%d:x=%d-text_w/2:y=%d" % (font_size,horizontal_position_timecode,vertical_position_timecode))
 	print  textoptions
 	
 	# Get starting timecode. In a raw state that requires further processing further on in the script.

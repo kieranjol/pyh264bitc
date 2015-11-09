faa256e51d9208b9a65ca9466af0173074f09343
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-09 21:21:28 +0000

    cleanup

--- py_h264_bitc.py
+++ py_h264_bitc.py
@@ -4,7 +4,8 @@ import os
 from glob import glob
 import pdb
 from sys import platform as _platform
- # Determine which operating system. Only important for locating bitc fonts.
+
+# Determine which operating system. Only important for locating bitc fonts.
 if _platform == "linux" or _platform == "linux2":
     print "linux"
 elif _platform == "darwin":
@@ -43,11 +44,14 @@ for filename in video_files: #loop all files in directory
 	# Calculate x and y coordinates of the timecode
 	vertical_position_timecode = video_height / 1.2
 	horizontal_position_timecode = video_width / 2
+	
 	# Calculate appropriate font size
 	font_size = video_height / 12
 	#pdb.set_trace()
+	
 	textoptions = ("fontsize=%d:x=%d-text_w/2:y=%d" % (font_size,horizontal_position_timecode,vertical_position_timecode))
 	print  textoptions
+	
 	# Get starting timecode. In a raw state that requires further processing further on in the script.
 	timecode_test_raw = subprocess.check_output(['ffprobe',
 									'-v', 'error',
@@ -63,6 +67,7 @@ for filename in video_files: #loop all files in directory
 									'-show_entries', 'stream=avg_frame_rate',
 									'-of', 'default=noprint_wrappers=1:nokey=1',
 									filename])
+									
 	# This tests if there is actually a timecode present in the file.								
 	if not timecode_test_raw:
 		# The timecode needs to be phrased in a way unique to each operating system. Note the backslashes.
@@ -91,6 +96,7 @@ for filename in video_files: #loop all files in directory
 		#pdb.set_trace()
 	# This removes the new line character from the framemrate.
 	fixed_framerate = get_framerate.rstrip()
+	
 	#all these prints are just for testing. Will be removed later.
 	print fixed_framerate	
 	drawtext_options = "drawtext=%s:fontcolor=white:timecode=%s:rate=%s:boxcolor=0x000000AA:box=1:%s" % (font_path,timecode_test, fixed_framerate, textoptions)

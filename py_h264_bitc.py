91df90d0c8dfd83d0cd9ab45520be6b04434fc9a
Kieran O'Leary <kieran.o.leary@gmail.com>
2015-11-09 21:11:40 +0000

    Added comments & changed starting TC when none present

--- py_h264_bitc.py
+++ py_h264_bitc.py
@@ -4,6 +4,7 @@ import os
 from glob import glob
 import pdb
 from sys import platform as _platform
+ # Determine which operating system. Only important for locating bitc fonts.
 if _platform == "linux" or _platform == "linux2":
     print "linux"
 elif _platform == "darwin":
@@ -47,6 +48,7 @@ for filename in video_files: #loop all files in directory
 	#pdb.set_trace()
 	textoptions = ("fontsize=%d:x=%d-text_w/2:y=%d" % (font_size,horizontal_position_timecode,vertical_position_timecode))
 	print  textoptions
+	# Get starting timecode. In a raw state that requires further processing further on in the script.
 	timecode_test_raw = subprocess.check_output(['ffprobe',
 									'-v', 'error',
 									'-select_streams', 'v:0',
@@ -54,24 +56,27 @@ for filename in video_files: #loop all files in directory
 									'-of', 'default=noprint_wrappers=1:nokey=1',
 									filename])
 	
+	# Get framerate so that bitc is accurate.
 	get_framerate = subprocess.check_output(['ffprobe',
 									'-v', 'error',
 									'-select_streams', 'v:0',
 									'-show_entries', 'stream=avg_frame_rate',
 									'-of', 'default=noprint_wrappers=1:nokey=1',
 									filename])
-									
+	# This tests if there is actually a timecode present in the file.								
 	if not timecode_test_raw:
-		
+		# The timecode needs to be phrased in a way unique to each operating system. Note the backslashes.
+		# This section makes up a timecode if none is present in the file.
 		if _platform == "darwin":
 		    print "OS X"
-		    timecode_test = '02\\\:00\\\:00\\\:00'
+		    timecode_test = '01\\\:00\\\:00\\\:00'
 		elif _platform == "win32":
 		    print "Windows"
 		    font_path = "fontfile='C\:\\\Windows\\\Fonts\\\\'arial.ttf'"
-		    timecode_test = '02\:00\:00\:00'
+		    timecode_test = '01\:00\:00\:00'
 			
 	else:
+		# If a timecode is present, this will modify the raw input so that it is compatible with each operating system.
 		if _platform == "darwin":
 		    print "OS X"
 		    timecode_test = timecode_test_raw.replace(':', '\\\:').replace('\n', '')
@@ -84,10 +89,11 @@ for filename in video_files: #loop all files in directory
 		
 	
 		#pdb.set_trace()
-	gd = get_framerate.rstrip()
-
-	print gd	
-	drawtext_options = "drawtext=%s:fontcolor=white:timecode=%s:rate=%s:boxcolor=0x000000AA:box=1:%s" % (font_path,timecode_test, gd, textoptions)
+	# This removes the new line character from the framemrate.
+	fixed_framerate = get_framerate.rstrip()
+	#all these prints are just for testing. Will be removed later.
+	print fixed_framerate	
+	drawtext_options = "drawtext=%s:fontcolor=white:timecode=%s:rate=%s:boxcolor=0x000000AA:box=1:%s" % (font_path,timecode_test, fixed_framerate, textoptions)
 	print drawtext_options
 	print timecode_test
 	print get_framerate

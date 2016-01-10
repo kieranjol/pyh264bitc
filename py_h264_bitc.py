c15ce53d01e5ac9171b2aa4064da76327566e223
Kieran O'Leary <kieran.o.leary@gmail.com>
2016-01-10 17:36:11 +0000

    Add Ubuntu support (thanks @retokromer)

--- py_h264_bitc.py
+++ py_h264_bitc.py
@@ -4,6 +4,7 @@ import os
 from glob import glob
 import pdb
 from sys import platform as _platform
+print _platform
 
 # Directory with files that we want to transcode losslessly and generate metadata for
 video_dir = sys.argv[1]
@@ -46,6 +47,12 @@ for filename in video_files: #loop all files in directory
         (font_size,horizontal_position_timecode,vertical_position_timecode))
         print "OS X"
         font_path= "fontfile=/Library/Fonts/AppleGothic.ttf"
+    elif _platform == "linux2":
+        textoptions = ("fontsize=%d:x=%d-text_w/2:y=%d" % 
+        (font_size,horizontal_position_timecode,vertical_position_timecode))
+        print "OS X"
+        font_path= "fontfile=/usr/share/fonts/truetype/freefont/FreeSerifBold.ttf"
+	
     elif _platform == "win32":
         textoptions = ("fontsize=%d:x=%d-text_w/2:y=%d'" % 
         (font_size,horizontal_position_timecode,vertical_position_timecode))
@@ -64,7 +71,7 @@ for filename in video_files: #loop all files in directory
 		# The timecode needs to be phrased in a way unique to each operating system.
 		# Note the backslashes.
 		# This section makes up a timecode if none is present in the file.
-        if _platform == "darwin":
+        if _platform == "darwin" or _platform == "linux2":
             print "OS X"
             timecode_test = '01\\\:00\\\:00\\\:00'
         elif _platform == "win32":
@@ -75,7 +82,7 @@ for filename in video_files: #loop all files in directory
     else:
 		# If timecode is present, this will escape the colons
 		# so that it is compatible with each operating system.
-        if _platform == "darwin":
+        if _platform == "darwin" or _platform == "linux2":
             print "OS X"
             timecode_test = timecode_test_raw.replace(':', '\\\:').replace('\n', '')
         elif _platform == "win32":
@@ -92,7 +99,7 @@ for filename in video_files: #loop all files in directory
     print fixed_framerate	
     drawtext_options = ("drawtext=%s:fontcolor=white:timecode=%s:\
     rate=%s:boxcolor=0x000000AA:box=1:%s,\
-    drawtext=%s:fontcolor=white:text='IFI Irish':%s" % 
+    drawtext=%s:fontcolor=white:text='INSERT WATERMARK TEXT HERE':%s" % 
     (font_path, timecode_test, fixed_framerate, textoptions, font_path, watermark_options))
     print drawtext_options
     print timecode_test
